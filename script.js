78dabf1c8cf1792478bfa630fa09ec738e8408d0
Yagobfe01 <yagobaltazarferreira@gmail.com>
2025-02-26 15:52:05 -0300

    Segundo Commit

--- script.js
+++ script.js
@@ -10,15 +10,36 @@ function adicionarTarefa() {
         checkbox.type = "checkbox";
         checkbox.classList.add("checkbox-tarefa");
 
+        //DEIXAR PRIMEIRA LETRA MAIUSCULA-------
+
         let textoFormatado = texto.split(" ") 
             .map(palavra => palavra.charAt(0).toUpperCase() + palavra.slice(1).toLowerCase()) 
             .join(" "); 
-
+        
         let textoTarefa = document.createElement("span");
         textoTarefa.textContent = textoFormatado;
 
+         let lixeira = document.createElement("img");
+         lixeira.src = "lixeira.png";
+         lixeira.alt = "Excluir Tarefa";
+         lixeira.classList.add("lixeira");
+
+        checkbox.addEventListener("change", function() {
+            if (checkbox.checked) {
+                textoTarefa.classList.add("riscado");
+            } else {
+                textoTarefa.classList.remove("riscado");
+            }
+        });
+
+         // Função para excluir o item quando a lixeira for clicada
+         lixeira.addEventListener("click", function() {
+            lista.removeChild(novaTarefa);
+        });
+
         novaTarefa.appendChild(checkbox);
         novaTarefa.appendChild(textoTarefa);
+        novaTarefa.appendChild(lixeira);
 
         lista.appendChild(novaTarefa);
         
